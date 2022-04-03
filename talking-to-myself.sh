d6d0df2e3ef3b0988baa74c980bda2b691cc147d
brianc2788 <brianc2788@gmail.com>
2022-04-03 16:05:49 -0400

    As noted in a comment in the script; 'just for fun.'

---
+++ talking-to-myself.sh
@@ -0,0 +1,11 @@
+# just for fun.
+# color-coded conversation with myself in the shell.
+# full-screen your terminal/emulator for best results.
+# brianc2788@gmail.com
+
+echo "Talking to myself..."
+tput setaf 1;tput cup 5 10;echo 'where am i?'
+tput setaf 2;tput cup 10 20;echo 'huh? who are you?'
+tput setaf 1;tput cup 15 10;echo 'how are you doing that?'
+tput setaf 2;tput cup 20 20;echo 'doing what?'
+tput setaf 10;tput cup 25 15;echo '...'
