31e3ed0a9dd70ede11e7385d51f85a2609c01679
brianc2788 <brianc2788@gmail.com>
2022-04-04 03:36:12 -0400

    Made 'talking-to-myself' script more funner!

--- talking-to-myself.sh
+++ talking-to-myself.sh
@@ -1,15 +1,32 @@
-# TALKING-TO-MYSELF.SH - SHELL SCRIPT
-# ===================================
-# just for fun. might lead to some good ideas for
-# a UI or something later on.
-# brianc2788@gmail.com
+# ============================================== #
+# TALKING-TO-MYSELF.SH - SHELL SCRIPT            #
+# ============================================== #
+# just for fun. might lead to some good ideas for#
+# something later on.                            #
+# clearly a lot of potential here.               #
+# -----------------------------------------------#
+# brianc2788@gmail.com                           #
+# ---------------------------------------------- #
 
 clear
+sleep 1.0
 echo "Talking to myself..."
+sleep 1
 tput setaf 1;tput cup 5 10;echo -n 'where am i?'
+sleep 1
 tput setaf 2;tput cup 10 20;echo -n 'huh? who are you?'
+sleep 1
 tput setaf 1;tput cup 15 10;echo -n 'how are you doing that?'
+sleep 1
 tput setaf 2;tput cup 20 20;echo -n 'doing what?'
-tput setaf 4;tput cup 25 15;echo -n '...'
+sleep 1
+tput setaf 4;tput cup 25 15;echo -n '.'
+sleep 1.5
+tput setaf 4;tput cup 25 16;echo -n '.'
+sleep 1.5
+tput setaf 4;tput cup 25 17;echo -n '.'
+sleep 1.5
+tput setaf 6;tput cup 34 1; echo -n "k. you can leave now."
+sleep 1
 tput setaf 6;tput cup 35 1;echo -n "press ENTER to exit..."
 read ${BLANKVAR}
