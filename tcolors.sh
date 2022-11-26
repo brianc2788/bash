8d99a72e0a0d9e665d092d7ff242bbda7818799d
brianc2788 <brianc2788@gmail.com>
2022-11-26 12:11:02 -0500

    Changed name from bashcolors.sh to tcolors.sh.

---
+++ tcolors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Color for bash prompt
+RED="\033[01;31m"
+GREEN="\033[01;32m"
+YELLOW="\033[01;33m"
+RESET="\033[00m"
+
+echo -e "${RED}red chars!"
+echo -e "${GREEN}green!"
+echo -e "${YELLOW}yella!"
+
