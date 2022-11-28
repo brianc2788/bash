6ed7fd10d2247f9fbbff7ce40e12a22c8851fcb8
brianc2788 <brianc2788@gmail.com>
2022-11-27 20:21:23 -0500

    Minor code edits, major comment rewrites.

--- brupdate.sh
+++ brupdate.sh
@@ -2,50 +2,46 @@
 # ######################################
 # brupdate.sh
 # -----------
-# Updates current BR record.
-# Updates working & master archives.
-# This script makes assumptions about
-# files, locations, paths, etc.
-# For personal use only.
-# Authored by brianc2788@gmail.com
+# Personal-use utility for use with
+# my "BR" stuff.
+# Requires brformat(.py)
+# http://brianc2788.github.io/
 # ######################################
 
 echo -n "Searching for BR record..."
 
+# BR file names are formatted as "BR-mmmYYYY.ext"
+# where m is 3-letter, lower-case month and Y is
+# a full, 4-digit year followed by an extension.
 BRTXT=$(ls | grep BR-*.txt)
 BRCSV=$(ls | grep BR-*\.csv)
 DEST="br-archives"
 MASTER="br-archives-master.tar.gz"
 
-# If non-null (as opposed to -z)
+# Check that BRTXT is non-null.
 if [ -n $BRTXT ];then
     echo "Found ${BRTXT}."
 else
-    echo "No results. Aborting."
+    echo "No BR text file found. Aborting."
 fi
 
-# Run "brformat" python script (from my $PATH). Creates csv.
-# Clone URL brformat: "https://www.github.com/user5260/pyscripts/brformat"
+# Run "brformat" python script (env path or working dir). Creates csv.
 echo -n "Formatting ${BRTXT} to CSV (${BRCSV}..."
 brformat ${BRTXT}
 echo "Done."
 
-# Make sure BRCSV isn't empty.
-# If it is, rerun same as in declaration.
-# Now that brformat has run, you are
-# guarenteed to have a csv present.
-# "-z" = null; i.e. "if nulli/empty, ..."
+# Check if csv was found earlier. If not, it should be there now.
 if [ -z $BRCSV ];then
     BRCSV=$(ls | grep BR-*\.csv)
 fi
 
-# Store .csv in variable and copy into archives.
-# Replace master archive.
+# Copy files into this year's dir. Fix hard-coded dir name.*
 echo -n "Copying to (${DEST})..."
 cp ${BRTXT} ${DEST}/br-2022
 cp ${BRCSV} ${DEST}/br-2022
 echo "Done."
 
+# Delete old master archives file and create a new one.
 echo -n "Replacing [${MASTER}]..."
 rm ${MASTER}
 tar -czf ${MASTER} ${DEST}
